f4eb66bf61fe48650f22b5985d3d7adceef00f0a
Dylan_Hermosillo <hermosillodylan@gmail.com>
2023-09-21 16:40:36 -0400

    Transfer of files to Git
    Uploading initial 00 py file and course descriptions file

---
+++ Python Files/00_Python_Intro.py
@@ -0,0 +1,73 @@
+'''
+Welcome to this introductory boot-camp for learning the Python programming language!
+
+We'll be going over some of the basics that will carry you into your first few courses here at Pima.
+
+We'll be looking at:
+- Pseudocode
+- Data Types
+- Variables
+- Loops
+- Functions
+- Object Oriented Programming
+
+Each of these python files will be follow-along programming projects written in pseudocode.
+
+Pseudocode is what a lot of your projects will look like in your courses. They're basic english instructions that provide
+a direction for your python programming.
+
+A quick example of this would be
+
+Pseudocode:
+1) Make a variable called classroom_a to represent the number of students and set the value to 15
+2) Make a variable called classroom_b and set the value to 25
+3) Using the two classroom variables, add them together and save it as total_students
+
+You then use these instructions to write your python code
+
+Python Code:
+classroom_a = 15
+classroom_b = 25
+total_students = classroom_a + classroom_b
+'''
+
+def main():
+    '''
+    Let's review some basic data types, and then create a few variables
+
+    integers (int): are numeric data types representing non-decimal values
+    floats (float): are numerical data types representing decimal values
+    strings (str): are character data types we use for words; strings of characters
+    boolean (bool): are values that represent true or false logic
+
+    Variables are ways to store information, much like how we use in math problems.
+    You can set the variable name and have it equal the data type you need it to be.
+
+    I.e:
+
+    integer_variable = 10
+    float_variable = 1.2341
+    string_variable = 'This is a string of characters'
+
+    Variables are great ways to use the same data repeatedly without making mistakes.
+    They also make it easy to update code -- as changing one variable used 500 times is easier than
+    changing 500 lines of code.
+
+    Now it's your turn! Use the pseudocode comments below to complete the program.
+
+    '''
+    # Let's add some integers together to get the sum of two numbers!
+    # Make two variables below called number1 and number2, assign them some integer values and let's get their sum.
+    # We'll start the first one for you!
+    number1 =  
+
+
+    print("*************")
+    print("The sum is:", (number1 + number2))
+    print("*************")
+
+    
+    
+
+if __name__ == '__main__':
+    main()
